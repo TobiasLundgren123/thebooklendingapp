06844d2a21a48436189b93356e61f69e8c89b486
TobiasLundgren123 <tobias_lundgren@yahoo.com>
2022-04-04 12:37:51 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/tobias/thebooklendingapp/ThebooklendingappApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.tobias.thebooklendingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebooklendingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThebooklendingappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/tobias/thebooklendingapp/ThebooklendingappApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.tobias.thebooklendingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebooklendingappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
